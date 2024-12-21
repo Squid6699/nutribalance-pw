6d97e216747391464e57cbba2fbe88cd5a864f8a
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-20 18:42:37 -0700

    create component select

---
+++ frontend/src/components/Select.tsx
@@ -0,0 +1,12 @@
+import "../css/Select.css";
+import { SelectProps } from "../types";
+
+function Select({}:SelectProps) {
+    return (
+        <>
+            <select name="" id=""></select>
+        </>
+    )
+}
+
+export default Select;
