09c9abda948acdf435c1036f293fdab7d5524c2a
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-20 18:42:09 -0700

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./pages/Login";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Select from "./components/Select";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/auth/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Select />} />
       </Routes>
     </>
   );
