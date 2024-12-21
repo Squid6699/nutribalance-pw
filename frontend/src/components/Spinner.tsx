273d2021fda139a9352e5e51f71e73eaa9d2e160
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-21 15:54:32 -0700

    spinner component

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import "../css/spinner.css";
+
+function Spinner(){
+    return (
+        <div className="spinner-container">
+          <div className="spinner"></div>
+        </div>
+    );
+}
+
+export default Spinner;
