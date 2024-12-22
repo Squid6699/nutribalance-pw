560ba0284fa93cb0996729e3905804ba7264b6e2
Squid6699 <brayanaviles96@gmail.com>
2024-12-22 00:41:46 -0700

    Home test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { useSesion } from "../hook/useSesion.ts";
 function Home() {
+    const { name, email } = useSesion();
     return (
         <div>
             <h1>Home</h1>
+            <span>{name}</span>
+            <span>{email}</span>
         </div>
     );
 }
