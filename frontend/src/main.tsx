2425c9d8b46286530e7de5e7b000c36bfd36c885
Squid6699 <brayanaviles96@gmail.com>
2024-12-22 00:41:31 -0700

    add sessionprovider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "react-query";
+import { SessionProvider } from "./context/session.tsx";
 // import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient({
@@ -20,10 +21,12 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        {/* <ReactQueryDevtools/> */}
-      </QueryClientProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          {/* <ReactQueryDevtools/> */}
+        </QueryClientProvider>
+      </SessionProvider>
     </BrowserRouter>
   </StrictMode>
 );
