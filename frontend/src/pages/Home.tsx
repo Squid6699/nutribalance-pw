5e4dc7698727cf3ac0d4cbd9296647b2d231175e
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-28 00:13:18 -0700

    features

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import "../css/home.css";
 // import { useSesion } from "../hook/useSesion.ts";
 import banner from "../assets/banner.jpg";
+import chef from "../assets/chef_recetas.png";
+import plan_dieta from "../assets/crear_plan_dieta.jpg";
+import receta_inteligente from "../assets/crear_receta_inteligente.jpg";
+
 import Input from "../components/Input";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -36,8 +40,54 @@ function Home() {
                 </div>
             </div>
 
-            <section>
-                <h1>Porque elegir Nutribalance</h1>
+            <section className="container-why">
+                <h1 className="title">Porque elegir Nutribalance</h1>
+                <span>
+                    Nutribalance es una aplicacion de nutricion y recetas que te ayuda a encontrar recetas
+                    saludables y dietas personalizadas. Con mas de 100,000 recetas, puedes estar seguro de
+                    que encontraras algo que te guste y se ajuste a tus necesidadees nutricionales.
+                </span>
+            </section>
+
+            <section className="container-features">
+                <div className="feature">
+                    <div className="feature-image">
+                        {/* <img src={chef} alt="Recetas saludables y deliciosas" /> */}
+                    </div>
+                    <span className="feature-title">
+                        Recetas saludables y deliciosas
+                    </span>
+                    <span className="feature-body">
+                        Descubre mas de 100,000 recetas saludables y deliciosas. Encuentra el tipo de receta que necesitas
+                        en funcion de tus objetivos nutricionales.
+                    </span>
+                </div>
+
+                <div className="feature">
+                    <div className="feature-image">
+                        {/* <img src={plan_dieta} alt="Crear tu plan de dieta personalizada" /> */}
+                    </div>
+                    <span className="feature-title">
+                        Crea tu plan de dieta personalizada
+                    </span>
+                    <span className="feature-body">
+                        Obten recomendaciones de comidas diarias personalizadas en funcion de tus objetivos
+                        nutricionales y preferencias personales.
+                    </span>
+                </div>
+
+                <div className="feature">
+                    <div className="feature-image">
+                        {/* <img src={receta_inteligente} alt="Creador de recetas inteligentes" /> */}
+                    </div>
+                    <span className="feature-title">
+                        Creador de recetas inteligentes
+                    </span>
+                    <span className="feature-body">
+                        Ingresa los ingredientes que tienes en casa y te diremos que recetas puedes hacer
+                        con ellos.
+                    </span>
+                </div>
             </section>
         </main>
     );
