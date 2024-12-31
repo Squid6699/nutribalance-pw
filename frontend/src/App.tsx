8675ff3281a83aee47553ec478bc15699f5843aa
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-31 00:35:50 -0700

    add profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
 
 function App() {
 
@@ -13,11 +14,12 @@ function App() {
       <Routes>
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
+        <Route path="/profile/view" element={<Profile />} />
         <Route path="/" element={<Home />} />
       </Routes>
-      <footer>
+      {/* <footer>
         <Footer/>
-      </footer>
+      </footer> */}
     </>
   );
 }
