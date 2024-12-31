71ff48d2bab9c869d79014bdb93bcdd50fc880aa
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-31 00:35:04 -0700

    create_recipes

--- backend/routers/create_recipes.js
+++ backend/routers/create_recipes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
 export const routerApiCreateRecipes = express.Router();
 
