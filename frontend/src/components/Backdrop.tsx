cee7c93e1d64e368f6ed658d27100151c77043d9
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-12-31 00:36:22 -0700

    add backdrop

---
+++ frontend/src/components/Backdrop.tsx
@@ -0,0 +1,13 @@
+import "../css/backdrop.css";
+
+function Backdrop() {
+    return (
+        <>
+            <div className="backdrop">
+                <div className="loader"></div>
+            </div>
+        </>
+    );
+}
+
+export default Backdrop;
